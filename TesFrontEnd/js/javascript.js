430123ef96beb75345ae73fcb63355bb2df95e20
eril19 <85010681+eril19@users.noreply.github.com>
2022-11-12 16:35:52 +0700

    Add files via upload

---
+++ TesFrontEnd/js/javascript.js
@@ -0,0 +1,29 @@
+
+function takePicture(value){
+    localStorage.setItem('key',value)
+    alert("Image Taken!")
+    location.replace('../html/changePhoto.html')
+}
+
+function setPicture(value){
+    localStorage.setItem('key',value)
+    alert("Image have been set to profile!")
+    location.replace('../html/index.html')
+}
+
+
+function checkImage(){
+    var simpanButtonBG = document.getElementById('buttonSimpan')
+    var simpanButton = document.getElementById('simpanButton')
+    var Image = document.getElementById('displayCapture') 
+
+    if(Image.style.backgroundImage == ""){
+        simpanButton.style.color = "#757575" 
+        simpanButtonBG.style.backgroundColor = "#E0E0E0"
+        simpanButton.disabled = true
+    }else{
+        simpanButton.style.color = "white" 
+        simpanButtonBG.style.backgroundColor = "#002984"
+    }
+}
+
